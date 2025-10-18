215f46d599475e2692710d5ed037d81a2f5447c4
natucia <natucia@list.ru>
2025-10-18 20:56:18 +0000

    Toomutch

--- faceid_workflow_integration.py
+++ faceid_workflow_integration.py
@@ -18,6 +18,8 @@ import replicate
 from telegram import Update
 from telegram.constants import ChatAction
 from telegram.ext import ContextTypes
+import logging
+logger = logging.getLogger()
 
 # === Константы/настройки ===
 REPLICATE_MODEL = "fofr/any-comfyui-workflow:latest"
